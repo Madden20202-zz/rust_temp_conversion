d58aaa797bde3c2874ac16a0dfef485b48160aab
madden20202 <business.mocksock@gmail.com>
2021-11-04 11:19:22 -0700

    added input to get scale value to change, not fix

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use std::io;
 fn main() {
-    let mut scale = String::new();
+    let scale = String::new();
     ask_scale(scale);
 }
 
 fn ask_scale(scale: String) {
+    let mut scale = String::new();
     println!("Will we be using Celcius or Fahrenheit?");
     println!("Input C or F");
-    
+    io::stdin()
+        .read_line(&mut scale)
+        .expect("Please input C or F");
 }
