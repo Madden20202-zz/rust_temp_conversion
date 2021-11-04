b29d00324352a0aa279a7602ee73d4bf827346f4
madden20202 <business.mocksock@gmail.com>
2021-11-04 12:12:06 -0700

    added documentation

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,19 @@ fn main() {
 }
 
 fn ask_scale() {
+
+    // adds a variable that lets 
+    // the formula be picked
     let mut scale = String::new();
+
+    // Prompts user
     println!("Will we be using Celcius or Fahrenheit?");
     println!("Input C or F");
+
+    // changes the scale value and gives an error message if not string
     io::stdin()
         .read_line(&mut scale)
         .expect("Please input C or F");
     println!("You put {}", scale);
+
 }
