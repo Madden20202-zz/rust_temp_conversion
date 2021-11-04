d59e5a5b2a799327b5678ab918a6b39792389d17
madden20202 <business.mocksock@gmail.com>
2021-11-04 11:31:49 -0700

    changed the scope of the scale variable

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use std::io;
+
 fn main() {
-    let scale = String::new();
-    ask_scale(scale);
+    ask_scale();
 }
 
-fn ask_scale(scale: String) {
+fn ask_scale() {
     let mut scale = String::new();
     println!("Will we be using Celcius or Fahrenheit?");
     println!("Input C or F");
     io::stdin()
         .read_line(&mut scale)
         .expect("Please input C or F");
+    println!("You put {}", scale);
 }
